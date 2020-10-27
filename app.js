31dc223e12e1d42f7f1fc3084b762139a2b387d9
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-10-27 21:38:59 +0800

    postgresql and mongo added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const { exec } = require('child_process')
-const { join } = require('path')
+const express = require('express');
+const { exec } = require('child_process');
+const { join } = require('path');
+const cors = require('cors');
 
 const port = process.env.PORT || 54774
-const app = express()
+const app = express();
 
+app.use(cors());
 app.use(express.static(join(__dirname, 'static')))
 
-app.get('/hello', (req, res) => {
-  exec(join(__dirname, 'shellfile.sh'), (err, stdout, stderr) => {
+
+app.get('/api/v2/mongoDB/status', (req, res) => {
+  exec(join(__dirname, '/mongoDB/shellfile.sh'), (err, stdout, stderr) => {
     if (err) {
       return res.status(400).json({ output: null, error: err.message })
     }
@@ -16,6 +19,70 @@ app.get('/hello', (req, res) => {
     res.status(200).json({ output: stdout, error: null })
   })
 })
+
+
+app.get('/api/v2/mongoDB/port', (req, res) => {
+  exec(join(__dirname, '/mongoDB/port.sh'), (err, stdout, stderr) => {
+    if (err) {
+      return res.status(400).json({ output: null, error: err.message })
+    }
+
+    res.status(200).json({ output: stdout, error: null })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/api/v2/postgres/status', (req, res) => {
+  exec(join(__dirname, '/postgres/shellfile.sh'), (err, stdout, stderr) => {
+    if (err) {
+      return res.status(400).json({ output: null, error: err.message })
+    }
+
+    res.status(200).json({ output: stdout, error: null })
+  })
+})
+
+app.get('/api/v2/postgres/start', (req, res) => {
+  exec(join(__dirname, '/postgres/start.sh'), (err, stdout, stderr) => {
+    if (err) {
+      return res.status(400).json({ output: null, error: err.message })
+    }
+
+    res.status(200).json({ output: stdout, error: null })
+  })
+})
+
+
+app.get('/api/v2/postgres/stop', (req, res) => {
+  exec(join(__dirname, '/postgres/stop.sh'), (err, stdout, stderr) => {
+    if (err) {
+      return res.status(400).json({ output: null, error: err.message })
+    }
+
+    res.status(200).json({ output: stdout, error: null })
+  })
+})
+
+app.get('/api/v2/postgres/port', (req, res) => {
+  exec(join(__dirname, '/postgres/port.sh'), (err, stdout, stderr) => {
+    if (err) {
+      return res.status(400).json({ output: null, error: err.message })
+    }
+
+    res.status(200).json({ output: stdout, error: null })
+  })
+})
+
 
 
 app.listen(port, () => {
